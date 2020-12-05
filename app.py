879f9b6bc61ad99163a7f767f9945abaceb1ed70
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-12-06 02:10:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-from Face_reality import check_reality
+from Test.Face_reality import check_reality
 from flask import Flask, Response, jsonify, request
-import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Default Message"
-
-
-@app.route('/video_feed', methods=["POST"])
-def video_feed():
+@app.route('/facereality', methods=["POST"])
+def faceReality():
     frame = request.form["image"]
 
     temp = check_reality(frame).returnLabel()
