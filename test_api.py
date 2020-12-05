7b273b5c367608b109dbb5bcc5103c4fa2b001a1
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-12-05 21:09:14 +0500

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,20 @@
+import os
+import base64
+from pathlib import Path
+import requests
+
+# Passing in a real face Image
+with open(str(Path(__file__).resolve().parent) + '/Images/1.png', 'rb') as img_file:
+    my_string = base64.b64encode(img_file.read())
+
+r = requests.post("http://0.0.0.0:5000/video_feed", data={'image': my_string})
+
+print(r.status_code)
+
+# Passing in a fake Image
+with open(str(Path(__file__).resolve().parent) + '/Images/57.png', 'rb') as img_file:
+    my_string = base64.b64encode(img_file.read())
+
+r = requests.post("http://0.0.0.0:5000/video_feed", data={'image': my_string})
+
+print(r.status_code)
